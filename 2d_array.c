8a0974611d393cf53d54257ba5dab1ba49111d93
VaishnaviKoduri <sri.vaishnavi.koduri@gmail.com>
2023-08-21 16:28:16 +0530

    adding a code

---
+++ 2d_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int arr[3][3];
+    int i,j,row,sum;
+    int min;
+    printf("Enter the elements:\n");
+    scanf ("%d%d",&i,&j);
+    for (i=0;i<=2;i++)
+    {
+
+        min =arr[i][0];
+        for (j=0;j<3;j++)
+        {
+        if (arr[i][j]<min);
+        }
+        printf ("Min:%d\n",min);
+    }
+}
